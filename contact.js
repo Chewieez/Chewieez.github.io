54c6ca09474036e8607c9134d448ec83c4d9e98d
Greg Lawrence <gregaudio@gmail.com>
2017-10-07 22:50:19 -0500

    added github info to contact database

--- contact.js
+++ contact.js
@@ -20,9 +20,17 @@ const emailInfo = {
     "icon": "/images/logo_gmail_48px.png"
 }
 
+const githubInfo = {
+    "socialPlatform": "GitHub",
+    "name": "Chewieez",
+    "url": "https://github.com/Chewieez",
+    "icon": "/images/GitHub-Mark-64px.png"    
+}
+
 contactInfo.twitterInfo = twitterInfo;
 contactInfo.linkedInInfo = linkedInInfo;
 contactInfo.emailInfo = emailInfo;
+contactInfo.githubInfo = githubInfo;
 
 let contactInfoString = JSON.stringify(contactInfo);
 localStorage.setItem("contactInfo", contactInfoString);
