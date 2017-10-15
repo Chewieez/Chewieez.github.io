0d14d9b2fb0e4a268edd632717643b5bf94f6a2f
Greg Lawrence <gregaudio@gmail.com>
2017-10-15 14:24:53 -0500

    edited styling on projects page, added separate css file for styles and created css folder, reorganized caption area

--- projects-controller.js
+++ projects-controller.js
@@ -20,15 +20,13 @@ for (key in retrievedAllProjects) {
                 <div class="thumbnail">
                     <a href="${project.image}" target="_blank"><img src="${project.image}" class="img-responsive" alt="${project.name}">
                         <div class="caption">
-                        <p>${project.name}</p>
-                        <p>Date completed: ${project.date}</p>
-                        <p>Technologies used: ${project.technologies}</p>
-                        <p>Teamates: ${project.teammates}</p>
+                        <h4>${project.name}</h4>
+                        <p><span class="important-text">Date completed:</span> ${project.date} &nbsp; &nbsp; <span class="important-text">Teammates:</span> ${project.teammates}</p>
+                        <p><span class="important-text">Technologies used:</span> ${project.technologies}</p>
                         </div>
                     </a>
                 </div>
-            </article>       
-    
+            </article>
         `
     }
 }
