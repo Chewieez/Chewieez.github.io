337fc54ece3fef49c5b497ebff2f5226b9943aa7
Greg Lawrence <gregaudio@gmail.com>
2017-10-19 16:00:03 -0500

    starting to create pagination code, may need to merge paginate.js with blog-controller.js

--- blog-controller.js
+++ blog-controller.js
@@ -1,33 +1,37 @@
-// selecting the HTML container to place my javascript code and assigning it to a variable
-let blogViewEl = document.getElementById("blog-view");
-let blogEntriesEl = document.getElementById("blog-entries-list"); 
-
-let importedBlog = JSON.parse(localStorage.getItem("blogPosts"));
-console.log("importedBlog", importedBlog)
+const importedBlog = JSON.parse(localStorage.getItem("blogPosts"));
 
 
-// looping over object (importedBlog) to get access to its content
-for (let key in importedBlog) {
-    let blogPostsArray = importedBlog[key];
+function postBlogs() {
+    // selecting the HTML container to place my javascript code and assigning it to a variable
+    let blogViewEl = document.getElementById("blog-view");
+    let blogEntriesEl = document.getElementById("blog-entries-list"); 
 
-// looping through the array of blog posts that was extraced from the object (importedBlog) 
-    for (let i = 0; i < blogPostsArray.length; i++) { 
-        let eachBlog = blogPostsArray[i];
-        
-// populating the html container with each blog post data 
-        blogViewEl.innerHTML += `
-        <article  id="blogPost-${eachBlog.id}">
-                <h4 class="blog-title">${eachBlog.title}</h4>
-                <p class="blog-subheading"><span class="special-text">by:</span> ${eachBlog.author}    <span class="special-text">published on:</span> ${eachBlog.published}</p>
-                <br>
-                <p class="blog-content">${eachBlog.content}</p>
-                <br>
-                <p class="blog-tags">tags: ${eachBlog.tags}</p>
-        </article>
-        <hr>
-        `
-        blogEntriesEl.innerHTML += `
-        <p><a href="#">${eachBlog.published}</a></p>
-        `
-    } 
+
+    // looping over object (importedBlog) to get access to its content
+    for (let key in importedBlog) {
+        let blogPostsArray = importedBlog[key];
+
+    // looping through the array of blog posts that was extraced from the object (importedBlog) 
+        for (let i = 0; i < blogPostsArray.length; i++) { 
+            let eachBlog = blogPostsArray[i];
+            
+    // populating the html container with each blog post data 
+            blogViewEl.innerHTML += `
+            <article  id="blogPost-${eachBlog.id}">
+                    <h4 class="blog-title">${eachBlog.title}</h4>
+                    <p class="blog-subheading"><span class="special-text">by:</span> ${eachBlog.author}    <span class="special-text">published on:</span> ${eachBlog.published}</p>
+                    <br>
+                    <p class="blog-content">${eachBlog.content}</p>
+                    <br>
+                    <p class="blog-tags">tags: ${eachBlog.tags}</p>
+            </article>
+            <hr>
+            `
+            blogEntriesEl.innerHTML += `
+            <p><a href="#">${eachBlog.published}</a></p>
+            `
+        } 
+    }
 }
+
+postBlogs();
---
+++ blog-pagination.js
@@ -0,0 +1,76 @@
+const blogPostsArray = importedBlog.blogEntries
+console.log("pagination blogPostsArray: ", blogPostsArray)
+
+
+const totalItems = blogPostsArray.length
+const itemsPerPage = 5
+const numberOfPages = Math.ceil(totalItems / itemsPerPage)
+const paginationEl = document.getElementById("blogPaginator")
+
+// Build the DOM string for the pagination links in the footer
+let paginationString = "<ul>"
+paginationString += "<a id='previous' href='#'>&lt;</a>"
+for (var i = 0; i < numberOfPages; i++) {
+    paginationString += ` <li><a class="blogPage page-${i+1}" href="#">${i+1}</a></li>`
+}
+paginationString += " <a id='next' class='page-2' href='#'>&gt;</a>"
+paginationString += "</ul>"
+
+// Update the DOM with the unordered list we just built
+paginationEl.innerHTML = paginationString
+
+// Store references to the next and previous arrows we just added
+const previousEl = document.getElementById("previous")
+const nextEl = document.getElementById("next")
+
+/*
+    Function that will be invoked each time the user clicks
+    on one of the pagination links at the bottom of the page
+*/
+function produceBlogs (event) {
+    // Clear the cheeses first before displaying the new ones
+    blogViewEl.innerHTML = ""
+
+    // Which number did the user click on?
+    const pageNumber = parseInt(
+        Array.from(event.target.classList)
+        .find(clazz => {
+            if (clazz.startsWith("page-")) return clazz
+        })
+        .split("-")[1]
+    )
+
+    // Change the class name of the previous arrow
+    if ((pageNumber - 1) === 0) {
+        previousEl.style.display = "none"
+    } else {
+        previousEl.style.display = "inline"
+        previousEl.className = `page-${pageNumber - 1}`
+    }
+
+    // Change the class name of the next arrow
+    if ((pageNumber + 1) > numberOfPages) {
+        nextEl.style.display = "none"
+    } else {
+        nextEl.style.display = "inline"
+        nextEl.className = `page-${pageNumber + 1}`
+    }
+
+    // Determine which items to display by slicing the array
+    const itemsToDisplay = blogPostArray.slice(
+        (pageNumber - 1) * itemsPerPage, 
+        pageNumber * itemsPerPage
+//     )
+
+//     // Display a <section> representation of each data object
+//     for (let i = 0; i < itemsToDisplay.length; i++) {
+//         let currentCheese = itemsToDisplay[i];
+//         cheeseEl.innerHTML += `
+//         <section class="${currentCheese.consistency}">
+//         <h1>${currentCheese.type}</h1>
+//         <h4>Is it smelly? ${currentCheese.smelly}</h4>
+//         <h4>It is ${currentCheese.consistency} to the touch</h4>
+//         </section>
+//         `
+//     }
+// }
