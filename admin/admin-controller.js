e937da8a9608d450b90c59fb5159c72d4db01139
Greg Lawrence <gregaudio@gmail.com>
2017-10-25 11:52:48 -0500

    reorganized blog-factory and moved code to create blog database inside an if statement and then in a function.

--- admin/admin-controller.js
+++ admin/admin-controller.js
@@ -1,9 +1,22 @@
-// move generator function into this file
-// move blog Factory function into this file
 
-// pull current blog database from local storage
-// parse blog database into variable
+
+// blog generator function and blog factory function are in the blog-factory.js file
+
+
+// pull current blog database from local storage and parse into variable
+const retrievedBlogDatabase = JSON.parse(localStorage.getItem("blogPosts")) || {}
+
+// Create `articles` key if it doesn't exist
+ProfessionalDatabase.blogArray = ProfessionalDatabase.blogArray || []
+
+
+// get control of DOM input elements
+let newBlogTitleEl = document.getElementById("admin-blog-title")
+let newBlogAuthorEl = document.getElementById("admin-blog-author")
+let newBlogContentEl = document.getElementById("admin-blog-content")
+let newBlogTagsEl = document.getElementById("admin-blog-tags")
 
 // get content out of each form field and assign to variable and then push data into the factory function
-// setup click event to run the function to take user input field content and generate a new blog post
 //  
+
+// setup click event to run the function to take user input field content and generate a new blog post
