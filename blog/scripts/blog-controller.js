19ab8688a7365f4699c9c11039fd2e862e30303f
Greg Lawrence <gregaudio@gmail.com>
2017-10-29 23:01:05 -0500

    added logic to change the text of the expand content button, to reflect whether the content has been expanded or not

--- blog/scripts/blog-controller.js
+++ blog/scripts/blog-controller.js
@@ -254,18 +254,29 @@ blogViewEl.addEventListener("click", expandContent)
 // create function to expand the content container for a blog post to show it's full contents on a button click
 // this function needs to live outside of the loadFullPage() function for scope, so we can call this function inside our search results function. 
 function expandContent(event) {
-    console.log("clicked event: ", event)
-    let clickedBtn = event.target.id 
+    //console.log("clicked event: ", event)
+    let clickedBtnId = event.target.id 
     
+    let clickedBtnEl = event.target;
+   // console.log("clickedBtnEl: ", clickedBtnEl.innerHTML)
+
     // parse out the number in the ID of the event that was clicked
-    const clickedBtnId = clickedBtn.split("-")[1];
+    const clickedBtnIdNum = clickedBtnId.split("-")[1];
     
     // check if the ID of event that was clicked starts with "expandContent-". If so, then expand that blog content
-    if (clickedBtn.startsWith("expandContent-")) {
+    if (clickedBtnId.startsWith("expandContent-")) {
         //assign the Id number of the clicked element to the blog content div to be expanded
-        let contentToExpand = document.getElementById("blogContent-" + clickedBtnId)
+        let contentToExpand = document.getElementById("blogContent-" + clickedBtnIdNum)
         // toggle the abridged class off when click, to then show the full content.
         contentToExpand.classList.toggle("abridged");
         
+        // check if the button clicked says "Click to see less" or "Click to see more" and toggle it's state. 
+        if (clickedBtnEl.innerHTML.includes("more")){
+            clickedBtnEl.innerHTML = "Click to see less"
+        }
+        else if (clickedBtnEl.innerHTML.includes("less")) {
+            clickedBtnEl.innerHTML = "Click to read more"
+        }
     }
+
 }
