df4a5201e100bd642ff0ccfe97ac602910fcccbc
Greg Lawrence <gregaudio@gmail.com>
2017-10-31 11:15:24 -0500

    moved fillSideColumnBlogList function outside of my loadFullPage function, fixed bad indentation and cleaned out some unneeded code.

--- blog/scripts/blog-controller.js
+++ blog/scripts/blog-controller.js
@@ -63,8 +63,9 @@ searchEl.addEventListener("keyup", function(event){
         // run function to populate the page with blogs
         loadFullPage()
 
+        // Further testing shows that I do not need this extra eventhandler line anymore
         // Create a new event listener for expanding content button to work. 
-        blogViewEl.addEventListener("click", expandContent) 
+        // blogViewEl.addEventListener("click", expandContent) 
     }
     
     else if (event.keyCode === 13) {
@@ -87,163 +88,142 @@ loadFullPage()
 
 
 function loadFullPage() {
-/* -- Start Pagination code -- */
-// assign the search results array to the totalItems for pagination
-let totalItems = blogSearchResults.length
-//const totalItems = blogPostsArray.length
+    /* -- Start Pagination code -- */
+    // assign the search results array to the totalItems for pagination
+    let totalItems = blogSearchResults.length
+    //const totalItems = blogPostsArray.length
 
-const itemsPerPage = 5
-let numberOfPages = Math.ceil(totalItems / itemsPerPage)
-const paginationEl = document.getElementById("blogPaginator")
+    const itemsPerPage = 5
+    let numberOfPages = Math.ceil(totalItems / itemsPerPage)
+    const paginationEl = document.getElementById("blogPaginator")
 
-// Build the DOM string for the pagination links in the footer
-let paginationString = "<ul>"
-paginationString += "<li><a id='previous' href='#'>&lt;</a></li>"
+    // Build the DOM string for the pagination links in the footer
+    let paginationString = "<ul>"
+    paginationString += "<li><a id='previous' href='#'>&lt;</a></li>"
 
-for (var i = 0; i < numberOfPages; i++) {
-    paginationString += ` <li><a class="blogPage page-${i+1}" href="#">${i+1}</a></li>`
-}
-// Go ahead and give the > character a class of page-2 on first page load
-paginationString += "<li><a id='next' class='page-2' href='#'>&gt;</a></li>"
-paginationString += "</ul>"
-
-// Update the DOM with the unordered list we just built
-paginationEl.innerHTML = paginationString
-
-// Store references to the next and previous arrows we just added
-const previousEl = document.getElementById("previous")
-const nextEl = document.getElementById("next")
-
-/*
-    Function that will be invoked each time the user clicks
-    on one of the pagination links at the bottom of the page
-*/
-function produceBlogs(event) {
-   
-    // Clear the list of blogs first before displaying the new ones
-    blogViewEl.innerHTML = ""
-
-    // Which number did the user click on?
-    // This function is creating a new array from dom token content in event.target.classList. Then using the .find function to search each string inside the array for "page-". If that exists, it splits the word into two pieces and places ii in a new array; ["page-", "1"] (whatever number that follows). Then the element at index[1] is converted into an integer with parseInt()
-    const pageNumber = parseInt(
-        Array.from(event.target.classList)
-        .find(clazz => {
-            if (clazz.startsWith("page-")) return clazz
-        })
-        .split("-")[1]
-    )
-
-
-    // Change the class name of the previous arrow
-    // Could use .style.visibility = "hidden" or "visible"
-    if ((pageNumber - 1) === 0) {
-        previousEl.style.visibility = "hidden"
-    } else {
-        previousEl.style.visibility = "visible"
-        previousEl.className = `page-${pageNumber - 1}`
+    for (var i = 0; i < numberOfPages; i++) {
+        paginationString += ` <li><a class="blogPage page-${i+1}" href="#">${i+1}</a></li>`
     }
+    // Go ahead and give the > character a class of page-2 on first page load
+    paginationString += "<li><a id='next' class='page-2' href='#'>&gt;</a></li>"
+    paginationString += "</ul>"
 
-    // Change the class name of the next arrow
-    if ((pageNumber + 1) > numberOfPages) {
-        nextEl.style.visibility = "hidden"
-    } else {
-        nextEl.style.visibility = "visible"
-        nextEl.className = `page-${pageNumber + 1}`
-    }
+    // Update the DOM with the unordered list we just built
+    paginationEl.innerHTML = paginationString
 
-    // Determine which items to display by slicing the array
-    const itemsToDisplay = blogSearchResults.slice(
-    // const itemsToDisplay = blogPostsArray.slice(       // code pre search input field
-        (pageNumber - 1) * itemsPerPage, 
-        pageNumber * itemsPerPage
-     )
+    // Store references to the next and previous arrows we just added
+    const previousEl = document.getElementById("previous")
+    const nextEl = document.getElementById("next")
 
-    // check if the array is empty, which would be the result of a search that didn't return any blogs. If array is empty, then populate blogview DOM element with a message to state no blogs were found matching query
-     if (itemsToDisplay.length === 0) {
-         blogViewEl.innerHTML = `
-            <p>No blog entries match your search parameters. </p>
-            `
-     }
+    /*
+        Function that will be invoked each time the user clicks
+        on one of the pagination links at the bottom of the page
+    */
+    function produceBlogs(event) {
+    
+        // Clear the list of blogs first before displaying the new ones
+        blogViewEl.innerHTML = ""
+
+        // Which number did the user click on?
+        // This function is creating a new array from dom token content in event.target.classList. Then using the .find function to search each string inside the array for "page-". If that exists, it splits the word into two pieces and places ii in a new array; ["page-", "1"] (whatever number that follows). Then the element at index[1] is converted into an integer with parseInt()
+        const pageNumber = parseInt(
+            Array.from(event.target.classList)
+            .find(clazz => {
+                if (clazz.startsWith("page-")) return clazz
+            })
+            .split("-")[1]
+        )
 
 
-    // Display a <section> representation of each data object by looping through the array of blog posts that was extracted from the object (retrievedBlogs) 
-    else {
-        itemsToDisplay.forEach(currentBlog => {
-            
-            let currentBlogPublishedDate = moment(parseInt(currentBlog.published)).format("dddd, MMMM Do YYYY")
-            
-            blogViewEl.innerHTML += `
-            <article  id="blogPost-${currentBlog.id}">
-                    <h4 class="blog-title">${currentBlog.title}</h4>
-                    <p class="blog-subheading"><span class="special-text">by:</span> ${currentBlog.author}    <span class="special-text">published on:</span> ${currentBlogPublishedDate}</p>
-                    
+        // Change the class name of the previous arrow
+        // Could use .style.visibility = "hidden" or "visible"
+        if ((pageNumber - 1) === 0) {
+            previousEl.style.visibility = "hidden"
+        } else {
+            previousEl.style.visibility = "visible"
+            previousEl.className = `page-${pageNumber - 1}`
+        }
+
+        // Change the class name of the next arrow
+        if ((pageNumber + 1) > numberOfPages) {
+            nextEl.style.visibility = "hidden"
+        } else {
+            nextEl.style.visibility = "visible"
+            nextEl.className = `page-${pageNumber + 1}`
+        }
+
+        // Determine which items to display by slicing the array
+        const itemsToDisplay = blogSearchResults.slice(
+        // const itemsToDisplay = blogPostsArray.slice(       // code pre search input field
+            (pageNumber - 1) * itemsPerPage, 
+            pageNumber * itemsPerPage
+        )
+
+        // check if the array is empty, which would be the result of a search that didn't return any blogs. If array is empty, then populate blogview DOM element with a message to state no blogs were found matching query
+        if (itemsToDisplay.length === 0) {
+            blogViewEl.innerHTML = `
+                <p>No blog entries match your search parameters. </p>
+                `
+        }
+
+
+        // Display a <section> representation of each data object by looping through the array of blog posts that was extracted from the object (retrievedBlogs) 
+        else {
+            itemsToDisplay.forEach(currentBlog => {
+                // use Moment.js to format published date and store in a variable
+                let currentBlogPublishedDate = moment(currentBlog.published).format("dddd, MMMM Do YYYY")
+                
+                blogViewEl.innerHTML += `
+                <article  id="blogPost-${currentBlog.id}">
+                        <h4 class="blog-title">${currentBlog.title}</h4>
+                        <p class="blog-subheading"><span class="special-text">by:</span> ${currentBlog.author}    <span class="special-text">published on:</span> ${currentBlogPublishedDate}</p>
+                        
+                        `
+                // check if the content for the blog post is created than 470 characters. If it is 
+                if (currentBlog.content.length > 470) {
+                    blogViewEl.innerHTML += `
+                    <div id="blogContent-${currentBlog.id}" class="abridged">
+                        <p class="blog-content">${currentBlog.content}</p>
+                    </div>
+                    <button id="expandContent-${currentBlog.id}" class="expandContentBtn">Click to read more</button>
                     `
-            // check if the content for the blog post is created than 470 characters. If it is 
-            if (currentBlog.content.length > 470) {
+                } else {
+                    blogViewEl.innerHTML += `
+                    <div id="blogContent-${currentBlog.id}">
+                        <p class="blog-content">${currentBlog.content}</p>
+                    </div>
+                    `
+                } 
+                
                 blogViewEl.innerHTML += `
-                <div id="blogContent-${currentBlog.id}" class="abridged">
-                    <p class="blog-content">${currentBlog.content}</p>
-                </div>
-                <button id="expandContent-${currentBlog.id}" class="expandContentBtn">Click to read more</button>
+                <p class="blog-tags">tags: ${currentBlog.tags}</p>
+                <hr>
+                </article>
                 `
-            } else {
-                blogViewEl.innerHTML += `
-                <div id="blogContent-${currentBlog.id}">
-                    <p class="blog-content">${currentBlog.content}</p>
-                </div>
-                `
-            } 
-            
-            blogViewEl.innerHTML += `
-            <p class="blog-tags">tags: ${currentBlog.tags}</p>
-            <hr>
-            </article>
-            `
-        })
+            })
+        }
     }
-}
 
-// Get the array of pagination anchor tags we added to the DOM
-const blogLinks = document.getElementsByClassName("blogPage")
+    // Get the array of pagination anchor tags we added to the DOM
+    const blogLinks = document.getElementsByClassName("blogPage")
 
-// Add event listeners to each <a> element in the pagination
-for (let j = 0; j < blogLinks.length; j++) {
-    let thisBlogEl = blogLinks[j];
-    thisBlogEl.addEventListener("click", produceBlogs);
-}
-
-// Initially show page one on first page load
-produceBlogs({
-    "target": {
-        "classList": ["page-1"]
+    // Add event listeners to each <a> element in the pagination
+    for (let j = 0; j < blogLinks.length; j++) {
+        let thisBlogEl = blogLinks[j];
+        thisBlogEl.addEventListener("click", produceBlogs);
     }
-})
 
-previousEl.addEventListener("click", produceBlogs)
-nextEl.addEventListener("click", produceBlogs)
-
-
-
-
-// create function to fill the side column of page with a list of all the blog posts, showing their published date
-function fillSideColumnBlogList() {
-    blogEntriesEl.innerHTML = ""
-blogPostsArray.forEach(currentBlogPost => {
-    let currentBlogPublishDate =  moment(parseInt(currentBlogPost.published)).format("dddd, MMMM Do YYYY")       
-    blogEntriesEl.innerHTML += `
-    <p><a href="#">${currentBlogPublishDate}</a></p>
-    `
+    // Initially show page one on first page load
+    produceBlogs({
+        "target": {
+            "classList": ["page-1"]
+        }
     })
-}
 
-fillSideColumnBlogList()
+    previousEl.addEventListener("click", produceBlogs)
+    nextEl.addEventListener("click", produceBlogs)
 
-
-// const blogViewEl = document.getElementById("blog-view");
-
-
-
-blogViewEl.addEventListener("click", expandContent)
+    blogViewEl.addEventListener("click", expandContent)
 
 }  // <-- End of loadFullPage() function
 
@@ -275,5 +255,18 @@ function expandContent(event) {
             clickedBtnEl.innerHTML = "Click to read more"
         }
     }
-
 }
+
+
+// create function to fill the side column of page with a list of all the blog posts, showing their published date
+function fillSideColumnBlogList() {
+    blogEntriesEl.innerHTML = ""
+blogPostsArray.forEach(currentBlogPost => {
+    let currentBlogPublishDate =  moment(currentBlogPost.published).format("dddd, MMMM Do YYYY")       
+    blogEntriesEl.innerHTML += `
+    <p><a href="#">${currentBlogPublishDate}</a></p>
+    `
+    })
+}
+
+fillSideColumnBlogList()
